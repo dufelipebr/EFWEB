43956a34313527220330323d4d75796250bb2e5c
Carlos Oliveira <du.felipe.br@gmail.com>
2025-06-20 14:54:44 -0300

    Setup

---
+++ Models/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using EFWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFWeb.Models
+{ 
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
---
+++ Models/TodoItemDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace EFWeb.Models;
+
+public class TodoItemDTO
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
